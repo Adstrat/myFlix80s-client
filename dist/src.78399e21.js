35fe959bd688e75d062ec5f8e5e02781ad52de37
Adstrat <73385073+Adstrat@users.noreply.github.com>
2021-07-21 08:11:46 +0200

    made visibility filter non-case sensitive

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -53448,7 +53448,7 @@ function MoviesList(props) {
 
   if (visibilityFilter !== '') {
     filteredMovies = movies.filter(function (m) {
-      return m.Title.includes(visibilityFilter);
+      return m.Title.toLowerCase().includes(visibilityFilter.toLowerCase());
     });
   }
 
@@ -55024,7 +55024,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59832" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53262" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
